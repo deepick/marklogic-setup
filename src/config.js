3e2078c7c531cd2629e411df04b2155c7b47f11a
Hans Huebner <hans.huebner@gmail.com>
2020-09-05 18:24:33 +0200

    Database name no longer hardcoded

--- src/config.js
+++ src/config.js
@@ -8,8 +8,6 @@ exports.db = {
     host: process.env.MARKLOGIC_HOST || 'localhost',
     port: process.env.MARKLOGIC_PORT || 8000,
     user: process.env.MARKLOGIC_USER || 'admin',
-    password: process.env.MARKLOGIC_PASSWORD || 'admin',
-    // FIXME: hard-coded database name
-    database: process.env.MARKLOGIC_DATABASE || 'kerndaten'
+    password: process.env.MARKLOGIC_PASSWORD || 'admin'
 };
 
